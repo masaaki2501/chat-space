f88f273ae192b36eca579b60d7c82b6ddad15b43
masaaki2501 <suyaberu33@gmail.com>
2020-03-19 19:00:24 +0900

    Revert "‘非同期のコミット’"
    This reverts commit 6780889267fb91606373b891164dab74f5098719.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -18,7 +18,6 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index, :create]
-  end
+  resources :groups, only: [:index, :new, :create, :edit, :update]
 end
